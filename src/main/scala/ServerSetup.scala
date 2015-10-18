3e64c147e8d690cd40e9e01f5bc54650029308f0
Waldemar Wosiński <kastoestoramadus@gmail.com>
2015-10-18 10:42:00 +0200

    simple routing added, latacy to go

--- src/main/scala/ServerSetup.scala
+++ src/main/scala/ServerSetup.scala
@@ -1,17 +1,25 @@
 import java.net.InetSocketAddress
 
 import com.twitter.finagle.builder.ServerBuilder
-import com.twitter.finagle.httpx.{Http, Request}
+import com.twitter.finagle.httpx.path._
+import com.twitter.finagle.httpx.{Method, HttpMuxer, Http, Request}
 import com.twitter.finagle.{Httpx, Service}
 import com.twitter.finagle.httpx
 import com.twitter.util.{Await, Future}
 
-class ServerSetup {
+object ServerSetup extends App{
   val service = new Service[httpx.Request, httpx.Response] {
-    def apply(req: httpx.Request): Future[httpx.Response] =
-      Future.value(
-        httpx.Response(req.version, httpx.Status.Ok)
-      )
+    def apply(req: httpx.Request): Future[httpx.Response] = {
+      Future.value {
+        (req.method, Path(req.path)) match {
+          case Method.Get -> Root =>
+            httpx.Response(req.version, httpx.Status.Ok)
+          case Method.Get -> Root / "foo" / "bar" =>
+            Thread.sleep(1000)
+            httpx.Response(req.version, httpx.Status.Ok)
+        }
+      }
+    }
   }
   val socketAddress = new InetSocketAddress(8000)
   val server = ServerBuilder()
