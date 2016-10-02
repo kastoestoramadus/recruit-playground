fdeae85354fbbb062006e86ab82956ab5707b191
Waldemar Wosiński <kastoestoramadus@gmail.com>
2016-10-02 10:30:37 +0200

    Updated tests for step 2.

--- src/test/scala/walidus/cshop/ShopTest.scala
+++ src/test/scala/walidus/cshop/ShopTest.scala
@@ -3,10 +3,13 @@ package walidus.cshop
 import org.scalatest.{FlatSpec, Matchers}
 
 class ShopTest extends FlatSpec with Matchers{
-  "Shop" should "match with example" in {
-    Shop.makeBill(Seq(Apple, Apple, Orange, Apple)) shouldEqual 205
+  "Shop" should "match with simple example" in {
+    Shop.makeBill(Seq(Apple, Orange)) shouldEqual 85
   }
-  "Shop" should "match with symmetric example" in {
-    Shop.makeBill(Seq(Orange, Orange, Apple, Orange)) shouldEqual 135
+  "Shop" should "give discount for oranges" in {
+    Shop.makeBill(Seq(Orange, Orange, Apple, Orange)) shouldEqual 110
+  }
+  "Shop" should "give free apple for one apple" in {
+    Shop.makeBill(Seq(Apple, Apple, Orange, Apple)) shouldEqual 145
   }
 }
